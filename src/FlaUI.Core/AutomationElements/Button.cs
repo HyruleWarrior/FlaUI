8cea6a7acf97e9dad1105e2086cea78fa42ab237
Jon Lee <jon.lee.0@gmail.com>
2017-09-10 22:09:33 -0700

    Reverting Button class to branched version.

--- src/FlaUI.Core/AutomationElements/Button.cs
+++ src/FlaUI.Core/AutomationElements/Button.cs
@@ -1,11 +1,11 @@
-﻿using System.Linq;
-using FlaUI.Core.Definitions;
-using FlaUI.Core.AutomationElements.PatternElements;
-using FlaUI.Core.Patterns;
+﻿using FlaUI.Core.AutomationElements.PatternElements;
 
 namespace FlaUI.Core.AutomationElements
 {
     public class Button : InvokeAutomationElement
     {
+        public Button(BasicAutomationElementBase basicAutomationElement) : base(basicAutomationElement)
+        {
+        }
     }
 }
